7e384bab6e02b12353af48307508e5663626e29b
Luis Felipe <felipelfoliveira@gmail.com>
2020-12-08 16:30:21 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/mscourse/hrworker/entities/Worker.java
@@ -0,0 +1,35 @@
+package com.mscourse.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_worker")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Worker implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	private Double dailyIncome;
+
+}
